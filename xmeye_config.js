d6f3ca7632b06a6e64f9490b2c623f42501b23a4
Peter Simeonov <peter@simeonov.de>
2021-11-16 18:50:36 +0100

    Fixed stream client

--- xmeye_config.js
+++ xmeye_config.js
@@ -1,38 +1,63 @@
 module.exports = function (RED) {
   'use strict'
-  var settings = RED.settings;
+  //?? let settings = RED.settings;
   const 
-    //xmeyecam = require('./lib/dvripclient'),
-    //sip?? utils = require('./utils');
+    xmeyecam = require('./lib/dvripclient')
+    //sip?? , utils = require('./utils')
+    ;
 
-  function XmeyeConfigNode(config) {
-    console.log('--- enter XmeyeConfigNode');
+  class XmeyeConfigNode {
+    constructor(config) {
+      RED.nodes.createNode(this, config);
 
-    RED.nodes.createNode(this, config);
+      this.ip = config.ip;
+      this.port = parseInt(config.port || 34567);
+      this.user = config.user || this.credentials.user;
+      this.password = config.password || this.credentials.password;
+      this.timeout = parseInt(config.timeout || 5000);
+      this.name = config.name;
+      
+      this.setMaxListeners(0); // by default only 10 listeners are allowed
 
-    this.ip = config.ip;
-    this.port = parseInt(config.port || 34567);
-    this.timeout = parseInt(config.timeout || 5000);
-    this.name = config.name;
-    
-    this.setMaxListeners(0); // by default only 10 listeners are allowed
+      this.on('close', this.onClose.bind(this));
 
-    this.on('close', function(done) {
+      this.access = null;
+    }
+
+    initialize() {
+      if (this.access) return;
+
+      if (!this.ip) {
+        this.error( "Cannot connect due to IP of the Xmeye device not configured", {} );
+           
+        this.access = null;
+        this.setStatus('unconfigured');
+        return;
+      }
+
+      this.setStatus('initializing');
+      this.access = new xmeyecam({ camIp: this.ip, camMediaPort: this.port, commandTimeoutMs: this.timeout });
+      
+      this.connect();
+    }
+
+    onClose(done) {
+      this.log('--- enter XmeyeConfigNode onClose');
       if (this.access) this.access.disconnect();
-      setXmeyeStatus(this, '');
+      this.setStatus(this, '');
       this.removeAllListeners('xmeye_status');
       if (done) done();
-    });
+    }
 
-    this.setXmeyeStatus = function (status) {
+    setStatus(status) {
+      this.log('--- enter XmeyeConfigNode setXmeyeStatus');
       this.xmeyeStatus = status;
       // Pass the new status to all listeners
       this.emit('xmeye_status', status);
     }
 
-    this.access = null; //sip++ new xmeyecam({ camIp: this.ip, camMediaPort: this.port, commandTimeoutMs: this.timeout });
-
-    this.connect = async function() {
+    async connect() {
+      this.log('--- enter XmeyeConfigNode connect');
       if (!this.access) {
         this.error('cam access object missing for ' + this.name);
         return;
@@ -40,23 +65,26 @@ module.exports = function (RED) {
     
       try {
         await this.access.connect();
-        this.log("-- Connected!");
+        this.setStatus('login');
+        this.log("Connected!");
     
-        await this.access.login({ Username: this.credentials.user, Password: this.credentials.password });
-        this.log("-- Logged in!");
+        await this.access.login({ Username: this.user, Password: this.password });
+        this.setStatus('connected');
+        this.log("Logged in!");
       }
       catch (e) {
-        this.log("Failed (3):", e);
         this.access.disconnect();
+        this.setStatus('disconnected');
+        this.log("Failed connect:" + e);
       }
     }
-    console.log('--- leave XmeyeConfigNode');
+ 
   }
 
   RED.nodes.registerType('xmeye-config', XmeyeConfigNode, {
     credentials: {
-      user: {type:"text"},
-      password: {type: "password"}
+      user: {type:'text'},
+      password: {type: 'password'}
     }
   });
 }
--- xmeye_device.js
+++ xmeye_device.js
@@ -1,269 +1,201 @@
 module.exports = function (RED) {
   'use strict'
-  var settings = RED.settings;
+  //sip?? let settings = RED.settings;
   //sip?? const utils = require('./utils');
 
-  function XmeyeDeviceNode(config) {
-    console.log('--- enter XmeyeDeviceNode');
-    console.log('--- config ' + config.toString());
+  class XmeyeDeviceNode {
+    constructor(config) {
+      RED.nodes.createNode(this, config);
 
-    RED.nodes.createNode(this, config);
+      this.deviceConfig = RED.nodes.getNode(config.deviceConfig);
+      this.action = config.action;
 
-    this.deviceConfig = RED.nodes.getNode(config.deviceConfig);
-    console.log('--- config.deviceConfig ' + config.deviceConfig);
+      this.on('input', this.onInput.bind(this));
+      this.on('close', this.onClose.bind(this));
 
-    this.action = config.action;
+      if (this.deviceConfig) {
+        // Start listening for xmeye config node status changes
+        this.deviceConfig.addListener('xmeye_status', this.onStatus.bind(this));
 
-    if (this.deviceConfig) {
-      // Start listening for xmeye config node status changes
-      this.deviceConfig.addListener('xmeye_status', this.onStatus);
+        // Show the current xmeye config node status already
+        this.onStatus(this.deviceConfig.xmeyeStatus);
 
-      // Show the current xmeye config node status already
-      //sip++ utils.setNodeStatus(this, 'device', this.deviceConfig.xmeyeStatus);
-
-      this.deviceConfig.initialize();
+        this.deviceConfig.initialize();
+      }
     }
 
-    this.on('input', function(msg, send, done) {
-      const action = this.action || msg.action;
+    onInput (msg, send, done) {
+      this.log('--- enter XmeyeDeviceNode onInput');
+      msg.topic = this.action || msg.topic; //TODO remove previous line
 
-      if (!action) {
-        this.warn('When no action specified in the node, it should be specified in the msg.myparam');
+      if (!msg.topic) {
+        this.log('--- msg: ' + JSON.stringify(msg));
+        this.warn('When no action specified in the node, it should be specified in the msg.topic');
         return;
       }
 
       try {
-        switch (action) {
-          case "queryFiles":
+        switch (msg.topic) {
+          case 'sendMessage':
+            this.sendMessage(msg, done);
+            return;
+          case 'OPFileQuery':
             this.queryFiles(msg, done);
             return;
+          case 'playback':
+            this.reqPlayback(msg, done);
+            return;
           default:
-            //node.status({fill:"red",shape:"dot",text: "unsupported action"});
-            if (done) {
-              done.error("Action " + action + " is not supported");                    
-              return;
-            }
-            else this.error("Action " + action + " is not supported");                    
+            if (done) done('Action ' + msg.topic + ' is not supported');                    
+            else this.error('Action ' + msg.topic + ' is not supported');                    
+            return;
         }
       }
       catch (exc) {
-        if (done) {
-          done.error("Action " + action + " failed: " + exc);
-          return;
-        }
-        else this.error("Action " + action + " failed: " + exc);
+        if (done) done('Action ' + msg.topic + ' failed: ' + exc);
+        else this.error('Action ' + msg.topic + ' failed: ' + exc);
+        return;
       }
+    }
 
-      if (done) done();
-    });
-
-    this.on('close', function(done) {
+    onClose(done) {
+      this.log('--- enter XmeyeDeviceNode onClose');
       if (this.listener) {
         this.deviceConfig.removeListener('xmeye_status', this.onStatus);
       }
       if (done) done();
-    });
-
-    this.onStatus = function(status) {
-      //sip++ utils.setNodeStatus(this, 'device', status);
     }
 
-    this.executeHelper = async function(msg, done) {
-      const resp = await this.access.queryFiles(msg.Command, msg.MessageName, msg.MessageData);
-      if (resp && resp.data) {
-        this.send({payload: resp.data});
+    onStatus(status) {
+      this.log('--- enter XmeyeDeviceNode onStatus ' + status);
+      switch(status) {
+        case 'unconfigured':
+          this.status({ fill: 'red', shape: 'ring', text: status});
+          break;
+        case 'initializing':
+        case 'login':
+          this.status({ fill: 'yellow', shape: 'dot', text: status});
+          break;
+        case 'connected':
+          this.status({fill: 'green', shape: 'dot', text: status}); 
+          break;
+        case 'disconnected':
+          this.status({ fill: 'red', shape: 'ring', text: status});
+          break;
+        case '':
+          this.status({});
+          break;
+        default:
+          this.status({fill: 'red', shape: 'ring', text: 'unknown'});
       }
-      if (done) done();
     }
 
-    this.queryFiles = async function(msg, done) {
-      const resp = await this.access.queryFiles(msg.begin, msg.end); //--('2020-06-26 00:00:00', '2020-06-26 23:59:59');
-      if (resp && resp.data) {
-        this.log("Record count " + resp.data.length);
-        resp.data.forEach(rec => {
-          // rec.FileName, rec.BeginTime, rec.EndTime, rec.FileLength
-
-    //     // Record count 1
-    //     //   name /idea0/2020-06-26/001/03.20.22-03.23.14[R][@41][0].h264
-    //     //   len 0x000012e8
-    //     //   begin 2020-06-26 03:20:22
-    //     //   end 2020-06-26 03:23:14
-    //     let album = {
-    //       name: rec.FileName,
-    //       //Exmpl. http://localhost:3000/cam13/sd/idea0/2020-06-26/001/03.20.22-03.23.14[R][@41][0].h264
-    //       doc: data.doc + rec.FileName,
-
-    //       // //Exmpl. http://localhost:3000/cam13/sd/rec?FileName=/idea0/2020-06-26/001/03.20.22-03.23.14[R][@41][0].h264&BeginTime=2020-06-26%2003:20:22&EndTime=2020-06-26%2003:23:14
-    //       // doc: data.doc + `/rec?FileName=${rec.FileName}&BeginTime=${rec.BeginTime}&EndTime=${rec.EndTime}`,
-    //       meta: {
-    //         line1: `size ${parseInt(rec.FileLength.substring(2), 16)}, begin ${rec.BeginTime}, end ${rec.EndTime}`
-    //       }
-    //     }
-    //     data.albums.push(album);
-        });
+    done(cb, msg = '') {
+      if (msg) {
+        if (cb) cb(msg);
+        else this.error('queryFiles failed:', e);
       }
       else {
+        if (cb) cb();
+      }
+    }
+
+    async sendMessage(msg, done) {
+      try {
+        if (!msg.payload) {
+          if (done) done('msg.payload not defined');
+          else this.error('msg.payload not defined');
+          return;
+        }
+        const resp = await this.deviceConfig.access.sendMessage(msg.payload);
+        if (resp) {
+          this.send({topic: msg.payload.MessageName, payload: resp});
+        }
+        this.done(done);
+      }
+      catch (e) {
+        this.done(done, 'sendMessage failed:' + e);
+      }
+    }
+
+    async queryFiles(msg, done) {
+      try {
+        const resp = await this.deviceConfig.access.executeHelper('FILESEARCH_REQ', 'OPFileQuery', {
+          BeginTime: msg.begin,
+          Channel: 0,
+          DriverTypeMask: '0x0000FFFF',
+          EndTime: msg.end,
+          Event: '*',
+          StreamType: '0x00000000',
+          Type: 'h264'
+        });
+    
+        if (resp && resp.data) {
+          //input in dataParser:
+          // { 
+          //   "Name" : "OPFileQuery",
+          //   "OPFileQuery" : [ 
+          //     {
+          //       "BeginTime" : "2021-11-11 17:29:53", 
+          //       "DiskNo" : 0, 
+          //       "EndTime" : "2021-11-11 17:29:57", 
+          //       "FileLength" : "0x00000307", 
+          //       "FileName" : "\/idea0\/2021-11-11\/001\/17.29.53-17.29.57[M][@53bd][0].h264", 
+          //       "SerialNo" : 0 
+          //     }
+          //   ],
+          //   "Ret" : 100, 
+          //   "SessionID" : "0x00000002" 
+          // }
+          this.send({topic: msg.topic, payload: resp.data});
+
+          this.log('Record count ' + resp.data.length);
+          resp.data.forEach(rec => {
+            // const FileName = rec.FileName;
+            // const BeginTime = new Date(Date.parse(rec.BeginTime));
+            // const EndTime = new Date(Date.parse(rec.EndTime));
+            // const FileLength = parseInt(rec.FileLength);
+          });
+        }
+        else {
+        }
+        if (done) done();
+      }
+      catch (e) {
+        this.done(done, 'queryFiles failed:' + e);
+      }
+    }
+
+    async reqPlayback(msg, done) {
+      // play sd record
+      if (!this.deviceConfig.access) {
+        this.done(done, 'cam access object missing for ' + this.deviceConfig.host);
+        return; //TODO throw
+      }
+      const record = msg.payload || {
+        FileName: '',
+        BeginTime: '',
+        EndTime: ''
+      };
+
+      try {
+        this.deviceConfig.access.on('videostream:lost', ()=>{
+          this.log('Cam videostream:lost');
+          this.deviceConfig.access.disconnectMedia();
+        });
+    
+        this.log('Try get stream!');
+        let { video, audio } = await this.deviceConfig.access.reqPlayback(record);
+        this.log('Got stream!');
+      }
+      catch (e) {
+        this.done(done, 'Failed (2):' + e);
+        this.deviceConfig.access.disconnectMedia();
       }
       if (done) done();
     }
 
-    // // play sd record
-    // xmeye_playback = async (cam, req, res) => {
-    //   if (!cam.access) {
-    //     this.error('cam access object missing for ' + cam.host);
-    //     return; //TODO throw
-    //   }
-
-    //   const server = req.app.srv;
-    //   if (!server) {
-    //     this.log('no server defined in req');
-    //     return; //TODO throw
-    //   }
-
-    //   try {
-    //     const opts = {
-    //       width: 640,
-    //       height: 480,
-    //     };
-        
-    //     //convert .264 file to .mp4:
-    //     //ffmpeg -framerate 24 -i input.264 -c copy output.mp4
-    //     //Frame rate is by default assumed to be 25. You can use the -r switch, e. g. -r 30 for 30 frames/second.
-
-    //     //For writing to stdout: -f avi pipe:1
-    //     const ffmpegargs = [
-    //       "-fflags", "+nobuffer+genpts",
-    //       "-analyzeduration", "1",
-    //       "-probesize", "32",
-    //       "-i", "pipe:", //read from stdin ("pipe:" == "pipe:0"), supported options: blocksize
-    //       "-c", "copy",
-
-    //       '-f', 'rawvideo',
-    //       '-'
-    //     ];
-    //     let streamer = new WebStreamerServer(server, opts, ffmpegargs, inst => {
-    //       streamers.delete(inst);
-    //       this.log('streamers count after delete is ' + streamers.size);
-    //     });
-
-    //     streamer.onstart = async()=>{
-    //       // this.log('ffmpeg started, now get record from cam');
-    //       let ffmpeg = streamer.ffmpeg;
-
-    //       try {
-    //         cam.access.on('videostream:lost', ()=>{
-    //           this.log('Cam videostream:lost');
-    //           cam.access.disconnect();
-    //         });
-        
-    //         let { video, audio } = await cam.access.reqPlayback(req.query, { Username: 'admin', Password: '888888' }, ffmpeg.stdin);
-    //         this.log('Got stream!');
-    //       }
-    //       catch (e) {
-    //         this.log('Failed (1):', e);
-    //         cam.access.disconnect();
-    //       }
-    //     };
-        
-    //     res.render('player', { doc: req.originalUrl });
-    //     streamers.add(streamer);
-    //     this.log('streamers count after add is ' + streamers.size);
-    //     //TODO start watchdog for start_feed
-    //   }
-    //   catch (e) {
-    //     this.log('Failed (2):', e);
-    //     cam.access.disconnect();
-    //   }
-    // }
-
-    // // play sd record
-    // xmeye_playback2 = async (cam, req, res) => {
-    //   if (!cam.access) {
-    //     this.error('cam access object missing for ' + cam.host);
-    //     return; //TODO throw
-    //   }
-
-    //   const server = req.app.srv;
-    //   if (!server) {
-    //     this.error('ERROR: No server defined in req');
-    //     return; //TODO throw
-    //   }
-
-    //   //Exmpl. /cam13/sd/idea0/2020-06-26/001/03.20.22-03.23.14[R][@41][0].h264
-    //   const parts = req.path.split('/');
-    //   if (parts.length < 7) {
-    //     this.error('ERROR: Playback path too short');
-    //     return;
-    //   }
-    //   if (parts[3] !== 'idea0') {
-    //     this.error('ERROR: Missing "idea0" in path');
-    //     return;
-    //   }
-
-    //   const FileName = [parts[0], parts[3], parts[4], parts[5], parts[6]].join('/');
-    //   const BeginTime = `${parts[4]} ${parts[6].substring(0, 8)}`;
-    //   const EndTime = `${parts[4]} ${parts[6].substring(9, 17)}`;
-
-    //   try {
-    //     const opts = {
-    //       width: 640,
-    //       height: 480,
-    //     };
-        
-    //     //convert .264 file to .mp4:
-    //     //ffmpeg -framerate 24 -i input.264 -c copy output.mp4
-    //     //Frame rate is by default assumed to be 25. You can use the -r switch, e. g. -r 30 for 30 frames/second.
-
-    //     //For writing to stdout: -f avi pipe:1
-    //     const ffmpegargs = [
-    //       "-fflags", "+nobuffer+genpts",
-    //       "-analyzeduration", "1",
-    //       "-probesize", "32",
-    //       "-i", "pipe:", //read from stdin ("pipe:" == "pipe:0"), supported options: blocksize
-    //       "-c", "copy",
-
-    //       '-f', 'rawvideo',
-    //       '-'
-    //     ];
-    //     let streamer = new WebStreamerServer(server, opts, ffmpegargs, inst => {
-    //       streamers.delete(inst);
-    //       this.log('streamers count after delete is ' + streamers.size);
-    //     });
-
-    //     streamer.onstart = async()=>{
-    //       // this.log('ffmpeg started, now get record from cam');
-    //       let ffmpeg = streamer.ffmpeg;
-
-    //       try {
-    //         cam.access.on('videostream:lost', ()=>{
-    //           this.log('Cam videostream:lost');
-    //           cam.access.disconnect();
-    //         });
-
-    //         let { video, audio } = await cam.access.reqPlayback(
-    //           { FileName, BeginTime, EndTime }, 
-    //           { Username: 'admin', Password: '888888' }, ffmpeg.stdin);
-    //         this.log('Got stream!');
-    //       }
-    //       catch (e) {
-    //         this.log('Failed (1):', e);
-    //         cam.access.disconnect();
-    //       }
-    //     };
-        
-    //     res.render('player', { doc: req.originalUrl });
-    //     streamers.add(streamer);
-    //     this.log('streamers count after add is ' + streamers.size);
-    //     //TODO start watchdog for start_feed
-    //   }
-    //   catch (e) {
-    //     this.log('Failed (2):', e);
-    //     cam.access.disconnect();
-    //   }
-    // }
-
-    console.log('--- leave XmeyeDeviceNode');
   }
 
-  RED.nodes.registerType("xmeye-device", XmeyeDeviceNode);
+  RED.nodes.registerType('xmeye-device', XmeyeDeviceNode);
 }
